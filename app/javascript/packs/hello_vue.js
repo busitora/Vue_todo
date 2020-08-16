47f8602cc60f6491c95b1c3356a25157a7da5ebe
busitora <ts12272530@gmail.com>
2020-08-16 13:04:01 +0900

    taskとtopを分割して表示できるようにした

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,16 +7,20 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import router from '../router'
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+		router,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
 })
+
+
 
 
 // The above code uses Vue without the compiler, which means you cannot
